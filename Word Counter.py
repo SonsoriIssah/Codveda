481353ae17f08a598a146bc34b6ff28542d790f8
SonsoriIssah <issonsori@gmail.com>
2026-02-21 17:49:55 +0000

    Improved; made it into a function ,avoided erros

--- Word Counter.py
+++ Word Counter.py
@@ -1,11 +1,17 @@
-count = 0
-with  open('words.txt','r') as f:
-    content = f.read().split()
+def word_counter(filename):#
+ try:#Avoid getting bugs for files not present
+     with  open(filename +'.txt','r') as f:
+      content = f.read().split()
+     count = [i for i in content if len(i)>1]
+     return len(count)
+ except FileExistsError as e:
+   print(e)
 
-for i in content:
-    if len(i) > 1:
-        count+= 1
 
-print(count)
+filename = input('Filename: ')
+print(word_counter(filename))
+
+
+
 
 
